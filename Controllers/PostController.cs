38892a1ed2998f80ee892ecbbe3630dc267058b6
Andy Wang <Zunyang.Wang@smu.ca>
2023-12-04 04:14:01 -0400

    added PostController stuff

--- Controllers/PostController.cs
+++ Controllers/PostController.cs
@@ -1,4 +1,8 @@
 ﻿using _5510_final_project_Forum.Data;
+using _5510_final_project_Forum.Models;
+using _5510_final_project_Forum.Models.ForumView;
+using _5510_final_project_Forum.Models.PostModels;
+using _5510_final_project_Forum.Models.ReplyModels;
 using Microsoft.AspNetCore.Mvc;
 
 namespace _5510_final_project_Forum.Controllers
@@ -13,11 +17,36 @@ namespace _5510_final_project_Forum.Controllers
         public IActionResult Index(int id)
         {
             var post = _postService.GetById(id);
+            var replies = BuildPostReplies;
             var model = new PostIndexModel
             {
+                Id = post.Id,
+                Title = post.Title,
+                AuthorId = post.User.Id,
+                AuthorName = post.User.UserName,
+                AuthorImageUrl = post.User.ProfileImageUrl,
+                AuthorRating = post.User.Rating,
+                Created = post.Created,
+                PostContent = post.Content,
+                Replies = replies
 
             };
             return View();
+        }
+
+        private IEnumberable<PostReplyModel> BuildPostReplies(IEnumerable<Replies> replies)
+        {
+            return replies.Select(reply => new PostReplyModel
+            {
+                Id = reply.Id,
+                AuthorName = reply.User.UserName,
+                AuthorId = reply.User.Id,
+                AuthorImageUrl = reply.User.ProfileImageUrl,
+                AuthorRating = reply.User.Rating,
+                Created = reply.Created,
+                ReplyContent = reply.Content,
+            });
+              
         }
     }
 }
---
+++ Models/ReplyModels/PostReplyModel.cs
@@ -0,0 +1,17 @@
+﻿using System.Data;
+
+namespace _5510_final_project_Forum.Models.ReplyModels
+{
+    public class PostReplyModel
+    {
+        public int Id { get; set; }
+        public string AuthorId { get; set; }
+        public string AuthorName { get; set; }
+        public string AuthorRating { get; set; }
+        public string AuthorImageUrl {  get; set; }
+        public DateTime Created {  get; set; }
+        public string ReplyContent { get; set; }
+        public int PostId { get; set; }
+
+    }
+}
